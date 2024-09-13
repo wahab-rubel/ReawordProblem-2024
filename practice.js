747e998b3e06199ba7313e616a430763a456317b
wahabshop <wahabmari751@gmail.com>
2024-09-13 16:44:56 +0600

    this my infinity

--- practice.js
+++ practice.js
@@ -51,10 +51,15 @@
 
 const points = [40, 100, 1, 5, 25, 10];
 
-for( let i = points.length -1; i >0; i--){
-  let j = Math.floor(Math.random() * (i+1));
-  let k = points[i];
-  points[i] = points[j];
-  points[j] = k;
+function myArrayMax(arr){
+  let len = points.length;
+  let max = -Infinity;
+  while (len--) {
+    if (arr[len] > max) {
+      max = arr[len];
+    }
+  }
+  return max;
 }
-
+const result = myArrayMax("points");
+console.log(result);
